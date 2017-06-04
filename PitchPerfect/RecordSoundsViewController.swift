c2556901ab6a0111590ad824177007c1b1f2136a
Oscar Garrucho <oscar.garrucho@gmail.com>
2017-06-04 19:13:25 +0200

    First commit

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,96 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Oscar Rodriguez Garrucho on 3/6/17.
+//  Copyright © 2017 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+
+    @IBAction func stopRecording(_ sender: Any) {
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to Record"
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+        
+    }
+    
+    
+    // MARK: - Audio Recorder Delegate
+    // implementamos el delegate de AVAudioRecorder aquí
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("finished recording")
+        if (flag){
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        }
+        else {
+            print("recording was not successful")
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# PitchPerfect Project #
+
+Proyecto a modo de ejemplo para un artículo de mi blog personal **sobre iOS && android**. 
+
+En esta entrada escribo sobre como grabar audio y reproducirlo después modificando ciertos valores como el `pitch` o el `rate` en iOS. En esta ocasión he decidido realizar el proyecto utilizando el lenguaje `Swift 3.0`.
+
+
+![alt tag1](img/img_1.png)
+![alt tag2](img/img_2.png)
+
+
+Para más información, visita mi blog: [www.oscargarrucho.com](http://oskarko.github.io/)
